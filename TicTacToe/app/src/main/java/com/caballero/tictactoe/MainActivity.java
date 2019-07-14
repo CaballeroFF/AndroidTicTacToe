75e99911f67d3b7611936e530e9248b0ffc55464
Francisco Caballero <caballero.csu@gmail.com>
2019-07-13 17:54:06 -0700

    saved instance

--- TicTacToe/app/src/main/java/com/caballero/tictactoe/MainActivity.java
+++ TicTacToe/app/src/main/java/com/caballero/tictactoe/MainActivity.java
@@ -4,11 +4,14 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -16,11 +19,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public static final int ROWS = 3;
     public static final String EMPTY = "empty";
     public static final String PLAYER1_TURN = "com.caballero.tictactoe.player1.turn";
-    public static final String TURN = "com.caballero.tictactoe.player1.turn";
-    public static final String PLAYER1_SCORE = "com.caballero.tictactoe.player1.turn";
-    public static final String PLAYER2_SCORE = "com.caballero.tictactoe.player1.turn";
+    public static final String TURN = "com.caballero.tictactoe.turn.key";
+    public static final String TURN_IMG = "com.caballero.tictactoe.turn.image";
+    public static final String PLAYER1_SCORE = "com.caballero.tictactoe.player1.score";
+    public static final String PLAYER2_SCORE = "com.caballero.tictactoe.player2.score";
+    public static final String BOARD_VALUES1 = "com.caballero.tictactoe.board.values1";
+    public static final String BOARD_VALUES2 = "com.caballero.tictactoe.board.values2";
+    public static final String BOARD_VALUES3 = "com.caballero.tictactoe.board.values3";
+
+    private static final String TAG = "MainActivity";
 
     private ImageView[][] imageViews = new ImageView[3][3];
+    private String[][] boardValues = new String[3][3];
     private ImageView turnImage, player1Img, player2Img;
     private TextView player1Text, player2Text;
     private Button resetButton;
@@ -44,18 +54,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onSaveInstanceState(outState);
         outState.putBoolean(PLAYER1_TURN, player1Turn);
         outState.putInt(TURN, turn);
+        outState.putInt(TURN_IMG, getTurnImage(player1Turn));
         outState.putInt(PLAYER1_SCORE, player1Score);
         outState.putInt(PLAYER2_SCORE, player2Score);
+        outState.putStringArray(BOARD_VALUES1, boardValues[0]);
+        outState.putStringArray(BOARD_VALUES2, boardValues[1]);
+        outState.putStringArray(BOARD_VALUES3, boardValues[2]);
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
 
-        player1Turn = savedInstanceState.getBoolean(PLAYER1_SCORE, true);
+        player1Turn = savedInstanceState.getBoolean(PLAYER1_TURN, true);
         turn = savedInstanceState.getInt(TURN, 0);
         player1Score = savedInstanceState.getInt(PLAYER1_SCORE, 0);
         player2Score = savedInstanceState.getInt(PLAYER2_SCORE, 0);
+        boardValues[0] = savedInstanceState.getStringArray(BOARD_VALUES1);
+        boardValues[1] = savedInstanceState.getStringArray(BOARD_VALUES2);
+        boardValues[2] = savedInstanceState.getStringArray(BOARD_VALUES3);
+        turnImage.setImageResource(savedInstanceState.getInt(TURN_IMG, R.drawable.ic_dot));
+        updateScores();
+        restoreBoardValues();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (isLegalMove(v)) {
+            if (player1Turn) {
+                ((ImageView) v).setImageResource(R.drawable.ic_dot);
+                v.setTag(R.drawable.ic_dot);
+                turnImage.setImageResource(R.drawable.ic_x);
+            } else {
+                ((ImageView) v).setImageResource(R.drawable.ic_x);
+                v.setTag(R.drawable.ic_x);
+                turnImage.setImageResource(R.drawable.ic_dot);
+            }
+            turn++;
+            evaluateBoard();
+            player1Turn = !player1Turn;
+            setBoardValues();
+        }
     }
 
     private void initViews() {
@@ -71,6 +111,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 String imageId = "button_" + row + col;
                 int resId = getResources().getIdentifier(imageId, "id", getPackageName());
 
+                boardValues[row][col] = EMPTY;
                 imageViews[row][col] = findViewById(resId);
                 imageViews[row][col].setOnClickListener(this);
             }
@@ -87,34 +128,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         updateScores();
     }
 
-    @Override
-    public void onClick(View v) {
-        if (isLegalMove(v)) {
-            if (player1Turn) {
-                ((ImageView) v).setImageResource(R.drawable.ic_dot);
-                v.setTag(R.drawable.ic_dot);
-                turnImage.setImageResource(R.drawable.ic_x);
-            } else {
-                ((ImageView) v).setImageResource(R.drawable.ic_x);
-                v.setTag(R.drawable.ic_x);
-                turnImage.setImageResource(R.drawable.ic_dot);
+    private int getTurnImage(boolean playerTurn) {
+        if (playerTurn) { 
+            return R.drawable.ic_dot;
+        }
+        return R.drawable.ic_x;
+    }
+
+    private void restoreBoardValues() {
+        for (int row = 0; row < ROWS; row++) {
+            for (int col = 0; col < COLS; col++) {
+                String stringId = boardValues[row][col];
+                imageViews[row][col].setTag(stringId);
+                if (!stringId.equals(EMPTY)) {
+                    int resId = Integer.parseInt(stringId);
+                    imageViews[row][col].setImageResource(resId);
+                } else {
+                    imageViews[row][col].setImageResource(R.drawable.ic_placeholder);
+                }
             }
-            turn++;
-            evaluateBoard();
-            player1Turn = !player1Turn;
+        }
+    }
+
+    private void setBoardValues() {
+        for (int row = 0; row < ROWS; row++) {
+            for (int col = 0; col < COLS; col++) {
+                String tag = imageViews[row][col].getTag().toString();
+                boardValues[row][col] = tag;
+            }
         }
     }
 
     private void evaluateBoard() {
-
         if (evaluateForWin()) {
-
             if (player1Turn) {
                 player1Wins();
             } else {
                 player2Wins();
             }
-
         } else if (turn >= 9) {
             draw();
         }
@@ -173,7 +224,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 field[i][j] = imageViews[i][j].getTag().toString();
             }
         }
-
+        //rows
         for (int i = 0; i < 3; i++) {
             if (field[i][0].equals(field[i][1])
                     && field[i][0].equals(field[i][2])
@@ -181,7 +232,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 return true;
             }
         }
-
+        //columns
         for (int i = 0; i < 3; i++) {
             if (field[0][i].equals(field[1][i])
                     && field[0][i].equals(field[2][i])
@@ -189,13 +240,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 return true;
             }
         }
-
+        //diagonal
         if (field[0][0].equals(field[1][1])
                 && field[0][0].equals(field[2][2])
                 && !field[0][0].equals(EMPTY)) {
             return true;
         }
-
+        //diagonal
         if (field[0][2].equals(field[1][1])
                 && field[0][2].equals(field[2][0])
                 && !field[0][2].equals(EMPTY)) {
@@ -223,7 +274,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         dialog.create().show();
     }
 
-    // TODO: 7/13/2019 saved state 
-    // TODO: 7/13/2019 landscape 
+    // TODO: 7/13/2019 landscape
     // TODO: 7/13/2019 AI 
 }
